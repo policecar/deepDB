4783ff5f00b1880fabd6d279c5dd290e7a880d3a
priska herger <priska@23bit.net>
2014-04-24 11:42:29 +0200

    temporary hackfix for num_words.

--- code/defaultParams.m
+++ code/defaultParams.m
@@ -3,6 +3,9 @@ if ~exist('params','var')
     params.init_no = 0; % 0 - random, 1 - Turian et al
     params.num_iter = 500; 
     params.train_both = 0;
+	
+	% number of unique words ( whereas num_entities may contain compounds )
+	params.num_words = 17905;
 
     params.batch_size = 20000;
     params.corrupt_size = 10;
